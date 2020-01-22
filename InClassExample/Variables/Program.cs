7148f20750ba946bc53a72485a2ea950ac48edcd
Matt Boensch <Matthew.D.Boensch-1@ou.edu>
2020-01-22 12:54:10 -0600

    Created variables for firstname, lastname, favoritenumber, also derrived crazy number

--- InClassExample/Variables/Program.cs
+++ InClassExample/Variables/Program.cs
@@ -11,6 +11,28 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Create a string named firstname
+            string FirstName;
+
+            //Assign a value to firstname (Use your name)
+            FirstName = "Matthew";
+
+            //Create an int named favoritenumber and assign your favorite number to it
+            int FavoriteNumber = 3;
+
+            double CrazyNumber = 7.6767 * FavoriteNumber;
+
+            string LastName = "Boensch";
+            string FullName = FirstName + " " + LastName;
+
+            //cw = Console.WriteLine()
+            Console.WriteLine(FullName + " Has the favorite number of " + FavoriteNumber 
+                + " \nwhich equates to a crazy number of " + CrazyNumber);
+
+            //MAKE IT DYNAMIC
+
+
+            Console.ReadKey();      
         }
     }
 }
